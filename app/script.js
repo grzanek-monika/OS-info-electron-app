daec33b51723f772082ac46a6860082c536712a3
Monika Grzanek <grzanekmonika@o2.pl>
2024-07-12 15:28:58 +0200

    Add main.js and add change in task runner

--- app/script.js
+++ app/script.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-undef */
 const os = require('os');
 
-const osPlatform = document.querySelector("#osPlatform");
-const osArchitecture = document.querySelector("#osArchitecture");
+window.addEventListener('DOMContentLoaded', () => { 
+    const osPlatform = document.querySelector("#osPlatform");
+    const osArchitecture = document.querySelector("#osArchitecture");
 
-osPlatform.innerHTML = `Platform: <strong>${os.platform()}</strong>`;
-osArchitecture.innerHTML = `Platform: <strong>${os.arch()}</strong>`;
+    osPlatform.innerHTML = `Platform: <strong>${os.platform()}</strong>`;
+    osArchitecture.innerHTML = `Platform: <strong>${os.arch()}</strong>`;
+})
+
